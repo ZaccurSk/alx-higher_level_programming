87c022b2c5a06c229b65e1ac1f76dde0500501f8
Skhumbuzo Zaccur Mahlangu <SkhumbuzoRakgoale@outlook.com>
2023-03-14 02:52:00 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-
-def arg_calc(argv):
-    n = len(argv) - 1
-    if n != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
-    if op == '+':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
-    elif op == '/':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
-    else:
+    a, b = int(argv[1]), int(argv[3])
+    operator = argv[2]
+    math = {"+": add, "-": sub, "*": mul, "/": div}
+    if operator not in math:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
-
-if __name__ == "__main__":
-    import sys
-    arg_calc(sys.argv)
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, math[operator](a, b)))
