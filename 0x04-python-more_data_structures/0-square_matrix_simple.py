d68690307ecdf8e7b1609e537727e5737f9ea612
Skhumbuzo Zaccur Mahlangu <SkhumbuzoRakgoale@outlook.com>
2023-03-16 01:27:24 +0200

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    squared = []
-     for line in matrix:
-         squared.append([c**2 for c in line])
-     return squared
+    """
+    wordA function that computes the square
+    value of all integers of a matrix.
+    """
+    new_matrix = []
+    for col in matrix:
+        result = list(map(lambda x: x**2, col))
+        new_matrix.append(result)
+    return new_matrix
